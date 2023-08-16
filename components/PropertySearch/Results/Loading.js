1e8db8a99e83b222edd9d5abe549016966c3c0f1
nicolaeciobota <ciobotanicolae@gmail.com>
2023-08-16 13:46:39 +0100

    suspense

---
+++ components/PropertySearch/Results/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Loading = () => {
+ return (
+  <section className="py-24">
+   <div className="container">
+    Loading...
+   </div>
+  </section>
+ )
+}
+export default Loading;
