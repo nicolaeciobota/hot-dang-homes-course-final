305bdd321380fefab2788f3e5ecdee6412374ade
nicolaeciobota <ciobotanicolae@gmail.com>
2023-09-12 11:00:02 +0100

    spinner-pink

--- app/loading.js
+++ app/loading.js
@@ -1,8 +1,8 @@
 const Loading = () => {
  return (
-  <section className="py-24"><div>Loading data....</div>
-   <span className="h-screen w-full flex justify-center items-center">
-      <span className="animate-spin relative flex h-10 w-10 rounded-sm bg-purple-400 opacity-75"></span>
+  <section className="py-24">
+   <span className="h-screen w-full flex justify-center items-center">Loading data....
+      <span className="animate-spin relative flex h-10 w-10 rounded-sm bg-pink-400 opacity-75"></span>
     </span>
   </section>
  )
